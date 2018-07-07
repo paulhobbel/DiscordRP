e2d2fb335f495e32901b73b1c4ea63d0645741f4
ItsDizzy <itsdizzy40@gmail.com>
2018-07-08 00:31:05 +0200

    Fixed startup crash

--- src/main/java/me/paulhobbel/discordrp/addons/ConfigPlugin.java
+++ src/main/java/me/paulhobbel/discordrp/addons/ConfigPlugin.java
@@ -14,8 +14,12 @@ public class ConfigPlugin implements IDiscordRPPlugin {
     public void register(IDiscordRPRegistry registry) {
         for(String stringDimension : DiscordRPConfig.customDimensions) {
             String[] parts = stringDimension.split("\\|");
-            Log.info("Registering config dimension with key '{}' and title '{}'", parts[0], parts[1]);
-            registry.registerDimension(new Dimension(parts[0], parts[1]), parts[0]);
+            if(parts.length == 2) {
+                Log.info("Registering config dimension with key '{}' and title '{}'", parts[0], parts[1]);
+                registry.registerDimension(new Dimension(parts[0], parts[1]), parts[0]);
+            } else {
+                Log.error("'{}' is not a correct format for a dimension, use <type>|<title> instead.", stringDimension);
+            }
         }
     }
 }
--- src/main/java/me/paulhobbel/discordrp/config/DiscordRPConfig.java
+++ src/main/java/me/paulhobbel/discordrp/config/DiscordRPConfig.java
@@ -13,7 +13,7 @@ public class DiscordRPConfig {
     @Config.Comment("Whether to show your elapsed time on Discord")
     public static boolean showTime = true;
 
-    public static String[] customDimensions = new String[] { "default;In A Dimension" };
+    public static String[] customDimensions = new String[] { "default|In A Dimension" };
 
     @Config.Comment("Manually set the manifest of this modpack, can be useful if you are not using a curse modpack")
     public static ManifestCategory manifest = new ManifestCategory();
